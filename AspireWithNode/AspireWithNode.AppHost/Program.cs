5e2e3a31a503eeba5f80d6ce170e690dcee0ca7a
Nicolas Clerc <nicolas.clerc@cellenza.com>
2024-01-23 14:15:46 +0100

    adding npm and eshoplite demo from AzureSample repos

---
+++ AspireWithNode/AspireWithNode.AppHost/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var weatherapi = builder.AddProject<Projects.AspireWithNode_AspNetCoreApi>("weatherapi");
+
+builder.AddNpmApp("frontend", "../NodeFrontend", "watch")
+    .WithReference(weatherapi)
+    .WithReference(cache)
+    .WithServiceBinding(containerPort: 3000, scheme: "http", env: "PORT")
+    .AsDockerfileInManifest();
+
+builder.Build().Run();
